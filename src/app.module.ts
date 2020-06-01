af07483347b4879bbd4c5863b03829a24590bf8b
voke1 <vokeolomu@yahoo.com>
2020-06-01 13:12:59 +0100

    integrate db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,45 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './users/users.controller';
+import { UserService } from './users/user.service';
 import { SmesController } from './smes/smes.controller';
+import { userSchema } from './users/schemas/user.schema';
+import { ConfigModule } from "@nestjs/config";
+import { ResponseService } from './utils/response-handler.service';
+import { TokenService } from './utils/jwt.service';
+
+
+const configuration = () => ({
+  port: parseInt(process.env.PORT, 10) || 8000,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  },
+});
+
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController, SmesController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: "User", schema: userSchema },
+    ]),
+
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
+
+  controllers: [
+    AppController,
+    UserController,
+    SmesController
+  ],
+  providers: [
+    AppService,
+    UserService,
+    ResponseService,
+    TokenService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
