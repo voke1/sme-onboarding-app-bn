f03277b7bf63e68bcd9b6eb2e271f61e9bdfbde3
voke1 <vokeolomu@yahoo.com>
2020-06-01 12:37:28 +0100

    create signin & signout route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Patch,
+    Body,
+    Req,
+    Res,
+    Param,
+    UseGuards,
+    Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface'
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/v1')
+export class UserController {
+    constructor(private readonly UserService: UserService) { }
+
+
+
+    @Post('auth/user/signup')
+    async Signup(
+        @Body() user: User,
+        @Req() req,
+        @Res() res,
+    ): Promise<User> {
+
+        return await this.UserService.signUp(user, res);
+    }
+
+
+    @Post('auth/user/signin')
+    async Signin(
+        @Body() user: User,
+        @Req() req,
+        @Res() res,
+    ): Promise<User> {
+        return await this.UserService.signIn(user, res);
+    }
+
+
+}
