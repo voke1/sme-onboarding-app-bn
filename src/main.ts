ee8d4726c15fcd8b00339c42f2ac6f2362daada7
voke1 <vokeolomu@yahoo.com>
2020-06-01 13:23:45 +0100

    fix heroku errors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 9000;
+  app.enableCors();
+  await app.listen(port);
+
 }
 bootstrap();
